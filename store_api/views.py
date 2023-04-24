5f2378ca47daf44c2160ac4bb09ff46e3035d639
elliotnguyen <nguyentienthanhgq@gmail.com>
2023-04-24 11:16:34 +0700

    add pricecompare, productrelevant api

--- store_api/views.py
+++ store_api/views.py
@@ -7,22 +7,15 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.renderers import JSONRenderer
-from django.db.models import CharField
-#from django.db.models.functions import Search
+from django.db.models import Q, Max
 
-#CharField.register_lookup()
 
-# class ProductList(generics.ListCreateAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-    
-#     def get_queryset(self):
-#         slug = self.kwargs['pk']
-#         if slug == '1':
-#            serializer = ProductSerializer(self.queryset)
-#            return Response(serializer.data)
-    
-    #def query_set(self,)
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
 class ProductList(APIView):
     def changeToNum(myString):
         myString = myString.replace(' ', '')
@@ -53,14 +46,65 @@ class ProductList(APIView):
             
             response_data["Promotion"] = Promotion.objects.get(ProdID = detail).Detail
             
+            from django.contrib.postgres.search import TrigramSimilarity
+            results = Product.objects.filter(Q(Type = response_data['Type']) & ~Q(ShopName = response_data['ShopName']))
+            results = results.annotate(similarity=TrigramSimilarity('ProductName', response_data['ProductName']),).filter(similarity__gte = 0.1).order_by('-similarity')[:20]
+
+            shopmapping = {
+                "FPTShop": 0,
+                "TGDD": 0,
+                "PhongVu": 0,
+                "CellPhoneS": 0
+            }
+            shopresults = {
+                "FPTShop": '',
+                "TGDD": '',
+                "PhongVu": '',
+                "CellPhoneS": ''
+            }
+            
+            del shopresults[response_data['ShopName']]
+            del shopmapping[response_data['ShopName']]
+
+            for item in results:
+                if item.similarity > shopmapping[item.ShopName]:
+                    shopmapping[item.ShopName] = item.similarity
+                    shopresults[item.ShopName] = item.ProductLink
+            
+            response_data['PriceCompare'] = shopresults
+            
+            results = {}
+            for item in Product.objects.filter(Q(Type = response_data['Type'])):
+                relevantRating = 0
+                for i in Configuration.objects.filter(ProdID = item.ProductID):
+                    if i.ConfigName in response_data['Configuration']:
+                        vectorizer = TfidfVectorizer()
+                        vectors = vectorizer.fit_transform([i.Detail, response_data['Configuration'][i.ConfigName]])
+                        relevantRating += cosine_similarity(vectors)[0][1]
+                results[item.ProductID] = relevantRating
+            
+            response_data['ProductRelevant'] = {}
+            sorted_dict = dict(sorted(results.items(), key=lambda item: item[1], reverse = True))
+            idx = 1
+            for key in sorted_dict:
+                if idx > 5:
+                    break
+                #khong chon cai dau tien boi vi cai dau tien se la san pham dang duoc xem
+                if idx > 1:
+                    tmp = Product.objects.filter(ProductID = key)
+                    response_data['ProductRelevant'][key] = {
+                        "ProductLink": tmp[0].ProductLink,
+                        "ImageLink": tmp[0].ImageLink
+                    }
+                idx += 1
+            
             return Response(response_data, status=status.HTTP_200_OK)
         
         search = request.GET.get('search')
         if search != None:
             search = " ".join(search.split('+'))
             from django.contrib.postgres.search import TrigramSimilarity
-            results = Product.objects.annotate(similarity=TrigramSimilarity('ProductName', search),).filter(similarity__gte=0.1).order_by('-similarity')
-            #results = Product.objects.filter(ProductName__search = search)
+            results = Product.objects.annotate(similarity=TrigramSimilarity('ProductName', search),).filter(similarity__gte=0.2).order_by('-similarity', '-SalePrice')
             dataList = []
             for item in results:
                 dataList.append(item)
@@ -168,100 +212,11 @@ class ProductList(APIView):
             }
             return Response(response_msg, status=status.HTTP_200_OK)
 
-# class ProductSearch(APIView):
-#     def get(self, request, *args, **kwargs):
-#            pass
-        
-        # ram = request.GET.get('ram')
-        # if ram != None:
-        #     for item in dataList:
-        #         tmp = Configuration.objects.get(ProdID = item.ProductID, ConfigName = "RAM")
-        #         if ram not in tmp.Detail.lower():
-        #             dataList.remove(item)
-        
-        # screen = request.GET.get('screen')
-        # if screen != None:
-        #     screen1 = screen + " inch"
-        #     screen2 = screen + "inch"
-        #     for item in dataList:
-        #         tmp = Configuration.objects.get(ProdID = item.ProductID, ConfigName = "Màn hình")
-        #         if not (screen1 in tmp.Detail.lower() or screen2 in tmp.Detail.lower()):
-        #             dataList.remove(item)
-        # cpu = request.GET.get('ra')
-        
-        
-        
-        
-        # if tmp == 'lap':
-        #     dataList = []
-        #     for item in Product.objects.filter(Type = "Máy tính cá nhân"):
-        #         dataList.append(item)
-        #     serializer = ProductSerializer(dataList, many=True)
-        #     return Response(serializer.data, status=status.HTTP_200_OK)
-        # elif id == 'dienthoai':
-        #     dataList = []
-        #     for item in Product.objects.filter(Type = "Điện thoại"):
-        #         dataList.append(item)
-        #     serializer = ProductSerializer(dataList, many=True)
-        #     return Response(serializer.data, status=status.HTTP_200_OK)
-        # elif id == 'tablet':
-        #     dataList = []
-        #     for item in Product.objects.filter(Type = "Máy tính bảng"):
-        #         dataList.append(item)
-        #     serializer = ProductSerializer(dataList, many=True)
-        #     return Response(serializer.data, status=status.HTTP_200_OK)
-        # #elif id == 'all'
 
 
 
-# class ProductDetail(APIView):
-#     def get(self, request, *args, **kwargs):
-#         id = self.kwargs['pk']
-        
-#         product = Product.objects.get(ProductID = id)
-#         #serializer = ProductSerializer(product)
-#         response_data = {}
-#         response_data["ProductName"] = product.ProductName
-#         response_data["BrandName"] = product.BrandName
-#         response_data["ShopName"] = product.ShopName
-#         response_data["ImageLink"] = product.ImageLink
-#         response_data["ProductLink"] = product.ProductLink
-#         response_data["SalePrice"] = product.SalePrice
-#         response_data["NormalPrice"] = product.NormalPrice
-#         response_data["Type"] = product.Type
-        
-#         temp = {}
-#         config = Configuration.objects.filter(ProdID = id)
-#         for item in config:
-#             temp[item.ConfigName] = item.Detail
-#         response_data["Configuration"] = temp
-        
-#         response_data["Promotion"] = Promotion.objects.get(ProdID = id).Detail
-        
-#         #response_data[]
-#         # from django.contrib.postgres.search import TrigramSimilarity
-#         # results = Product.objects.annotate(similarity=TrigramSimilarity('ProductName', product.ProductName),).filter(similarity__gte=0.1).order_by('-similarity')
-#         # results = Product.objects.filter(ProductName__search=product.ProductName).order_by('SalePrice')[:10]
-#         # for item in results:
-#         #     if item.ShopName == product.ShopName:
-#         #         continue
-#         #     response_data["ProdCompare"]["ProductName"] = item.ProductName
-#         #     response_data["ProdCompare"]["ProductLink"] = item.ProductLink
-#         #     response_data["ProdCompare"]["SalePrice"] = item.SalePrice
-        
-#         return Response(response_data, status=status.HTTP_200_OK)
-    
         
         
 
         
 
-# class LaptopList(generics.ListCreateAPIView):
-#     queryset = Product.objects.filter(Type = 'Máy tính cá nhân')
-#     serializer_class = ProductSerializer
-    
-# class PhoneList(generics.ListCreateAPIView):
-#     queryset = Product.objects.filter(Type = 'Điện thoại')
-#     serializer_class = ProductSerializer
-
-#class ProductDetail()
