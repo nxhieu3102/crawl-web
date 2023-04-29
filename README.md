d2095b687af31fbd7e74006940aaf8dfc5c9fa51
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-04-29 14:14:10 +0700

    add cors

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 A website using Scrapy to crawl product price in many e-commerce websites to make a comparison of a product being bought in different pages
 Project is built with the integration of Scrapy, Django and React
 
+Start docker-desktop 
 To run the project:
     python3 manage.py runserver
 
--- project02/settings.py
+++ project02/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     
     #django app
     'store',
@@ -53,10 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'project02.urls'
-
+CORS_ORIGIN_ALLOW_ALL = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
